4e174edbf27a66d0fc7373e6f9e76dada7ca725b
Jon Sykes <jsykes@fanatics.com>
2016-09-22 14:55:47 -0400

    Socket.IO Client Implementation
    Implemented Socket.IO on the client;
    Cleaned up old API calls on the client;
    Fixed Rabbit reciever.js to point to new buckets and added Socket.IO
    Emit calls to the components sockets.
    Fixed some code in the socket manager. Error handling still needs to be
    added to the socket manager.

--- server/managers/socketManager.js
+++ server/managers/socketManager.js
@@ -19,56 +19,67 @@ const initialize = (io) => {
                 console.log("Socket Server: Retrieved " + results.length + " templates from data store.");
                 io.emit('templates', results);
             });
-            
+            console.log("Current Socket Connection Count: " + io.engine.clientsCount);
         });
-
+        socket.on('components', function() 
+        {
+            console.log('Socket Server: Retrieving Components');
+            componentLogic.getAllComponents(function(error, results) 
+            {
+                console.log("Socket Server: Retrieved " + results.length + " components from data store.");
+                io.emit('components', results);
+            });
+            console.log("Current Socket Connection Count: " + io.engine.clientsCount);
+        });
         socket.on('componentByID', function(id) 
         {
             console.log('Socket Server: Retrieving component for ID: ' + id + '.');
-            componentLogic.getComponentDataByID(id, (error, results) => {
+            componentLogic.getComponentByID(id, (error, results) => {
                 var resObj = results[0];
                 var retVal = resObj[Object.keys(resObj)[0]];
                 
                 console.log('Socket Server: Retrieved Component for ' + id + '.');
-                io.emit('componentsByID', retVal);
+                console.log(retVal);
+                io.emit('componentByID', retVal);
             });
-            
+            console.log("Current Socket Connection Count: " + io.engine.clientsCount);
         });
 
         socket.on('componentsByContext', function(context) 
         {
             console.log('Socket Server: Retrieving components for context: ' + context + '.');
-            componentLogic.getComponentDataByContext(context, (error, results) => {                
+            componentLogic.getComponentByContext(context, (error, results) => {                
                 console.log('Socket Server: Retrieved components for context: ' + context + '.');
                 io.emit('componentsByContext', results);
             });
-            
+            console.log("Current Socket Connection Count: " + io.engine.clientsCount);
         });
 
         socket.on('componentsByTemplate', function(template) 
         {
             console.log('Socket Server: Retrieving components for template: ' + template + '.');
-            componentLogic.getComponentDataByTemplate(template, (error, results) => {                
+            componentLogic.getComponentByTemplate(template, (error, results) => {                
                 console.log('Socket Server: Retrieved components for template: ' + template + '.');
                 io.emit('componentsByTemplate', results);
             });
-            
+            console.log("Current Socket Connection Count: " + io.engine.clientsCount);
         });
 
         socket.on('componentsByTemplateContext', function(template, context) 
         {
             console.log('Socket Server: Retrieving components for template: ' + template + ' and context: ' + context + '.');
-            componentLogic.getComponentDataByTemplateContext(template, context, (error, results) => {               
+            componentLogic.getComponentByTemplateContext(template, context, (error, results) => {               
                 console.log('Socket Server: Retrieved components for template: ' + template + ' and context: ' + context + '.');
                 io.emit('componentsByTemplateContext', results);
             });
-            
+            console.log("Current Socket Connection Count: " + io.engine.clientsCount);
         });
 
         socket.on('test', function(msg) 
         {
             console.log('Test got a message: ' + msg);
             io.emit('test', msg);
+            console.log("Current Socket Connection Count: " + io.engine.clientsCount);
         });
     });
 }
